030a49394e3ee36fad3ea422eab36941368f29f3
Mateusz Majewski <matimaj2004@gmail.com>
2025-03-23 10:06:49 +0100

    Finished and tested whole code

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,14 +1,12 @@
 #include <Arduino.h>
-
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
-#include <Fonts/FreeMonoBold9pt7b.h> // Import a 12-pixel-high font
+#include <Fonts/FreeMonoBold9pt7b.h> // 12-pixel-high font
 #include <Adafruit_SSD1306.h>
 #include <string.h>
 
-#include "ssd1306_test.h"
-
+// Constatnt defines
 #define CH1_MEASURE_PIN A0
 #define CH2_MEASURE_PIN A1
 #define CH3_MEASURE_PIN A2
@@ -17,23 +15,19 @@
 #define CH3_ACTIVATE_PIN 7
 #define MEASURE_PERMIT_PIN 8
 #define BUTTON_PIN 2
+#define OLED_RESET -1
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 #define SMALL_CHAR_WIDTH 6
 #define SMALL_CHAR_HEIGHT 8
 #define BIG_CHAR_WIDTH 11
 #define BIG_CHAR_HEIGHT 12
-#define LONG_PRESS_DELAY 800
+#define LONG_PRESS_DELAY 300
 #define MIN_ACCEPTED_CHARGE 5 //%
-#define RELAY_SWITCHING_TIME 10 //ms
+#define RELAY_SWITCHING_TIME 8 //ms
+#define NO_CHARGE_MESSAGE_DISPLAY_TIME 1300
 
-// Button interrupt flag
-volatile bool buttonPressed = false;
-
-// Button interrupt flag function
-void buttonISR() { 
-    buttonPressed = true;
-    lastTime = millis();
-}
-
+// Battery graphic
 const uint8_t battery_bitmap[7][8] PROGMEM = {{
     0b01110000, 
     0b11111000, 
@@ -99,6 +93,16 @@ const uint8_t battery_bitmap[7][8] PROGMEM = {{
     0b11111000
 }};
 
+// Global structs and variables definitons
+struct Channel {
+    bool selected;
+    long int value;
+    long int charge;
+};
+Channel channel[3] = {{0, 1150, 0}, 
+                      {0, 1010, 0},
+                      {1, 680, 0}};
+
 bool channel_cycle[7][3] = {
     {0,0,1},
     {0,1,0},
@@ -108,36 +112,28 @@ bool channel_cycle[7][3] = {
     {1,1,0},
     {1,1,1}
 };
-uint8_t curr_channel_cycle = 0;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+bool activated = false;                 // Channel activation flag
+uint8_t curr_channel_cycle = 0;         // Keeps track of current channel cycle
+long int currTime, lastTime;            // Button variables to determine long or short press
+volatile bool buttonPressed = false;    // Button interrupt flag
 
-class Channel {
 
-public:
-    bool selected;
-    long int value;
-    long int charge;
+// Button interrupt flag function
+void buttonISR() { 
+    buttonPressed = true;
+    lastTime = millis();
+}
 
-    Channel(bool s, int v, int ch){
-        selected = s;
-        value = v;
-        charge = ch;
-    }
-};
 
-Channel channel[3] = {{0, 1150, 7}, 
-                      {1, 1010, 42},
-                      {0, 680, 70}};
-
-bool activated = false;
-
-long int currTime, lastTime;
-
+// Function calculates x posiotn of a string printed on screen 
 int calcStrPosX(char* str, uint8_t size, uint8_t channel){
     //          counting column pixel                    counting offset to left
     return ((SCREEN_WIDTH*(channel*2-1)/6)-((size?BIG_CHAR_WIDTH:SMALL_CHAR_WIDTH)*(strlen(str))/2)-1);
 }
 
 
+// Function updates information on the display
 void update_display(){
     // Set up text
     display.clearDisplay();
@@ -170,7 +166,7 @@ void update_display(){
     // Display channel capacitance values
     for(int i = 1; i <= 3; i++){
         char str[10];
-        sprintf(str, "%du", channel[i-1].value);
+        sprintf(str, "%du", (int)channel[i-1].value);
 
         display.setCursor(calcStrPosX(str, 0, i), 4*SMALL_CHAR_HEIGHT);
         display.print(str);   
@@ -179,22 +175,22 @@ void update_display(){
     // Display total expected charge
     display.setFont(&FreeMonoBold9pt7b);
     char str[10];
-    int total_expected_charge = (channel[0].value*channel[0].charge/100)+
-                                (channel[1].value*channel[1].charge/100)+
-                                (channel[2].value*channel[2].charge/100);
-    sprintf(str, "%du", total_expected_charge);
+    int total_expected_charge = (channel[0].selected?(channel[0].value*channel[0].charge/100):0)+
+                                (channel[1].selected?(channel[1].value*channel[1].charge/100):0)+
+                                (channel[2].selected?(channel[2].value*channel[2].charge/100):0);
+    sprintf(str, "%du", (int)total_expected_charge);
     display.setCursor(calcStrPosX(str, 1, 2), SCREEN_HEIGHT-4);
     display.print(str);
 
     // Display selected channels
     for(int i = 1; i <= 3; i++){
         if(channel[i-1].selected){
-            display.fillRect(SCREEN_WIDTH*(i-1)/3, 0, SCREEN_WIDTH/3, SCREEN_HEIGHT-BIG_CHAR_HEIGHT-8, SSD1306_INVERSE);
+            display.fillRect(ceil(SCREEN_WIDTH*(i-1)/3.0), 0, ceil(SCREEN_WIDTH/3.0), SCREEN_HEIGHT-BIG_CHAR_HEIGHT-8, SSD1306_INVERSE);
 
-            // Channel wrapped in rect while active
+            // Channel wrapped in rect while actives
             if(activated){
-                display.drawRect((SCREEN_WIDTH*(i-1)/3)+1, 1, SCREEN_WIDTH/3-2, SCREEN_HEIGHT-BIG_CHAR_HEIGHT-10, SSD1306_INVERSE);
-                display.drawRect((SCREEN_WIDTH*(i-1)/3)+2, 2, SCREEN_WIDTH/3-4, SCREEN_HEIGHT-BIG_CHAR_HEIGHT-12, SSD1306_INVERSE);
+                display.drawRect(ceil(SCREEN_WIDTH*(i-1)/3.0)+1, 1, SCREEN_WIDTH/3-2, SCREEN_HEIGHT-BIG_CHAR_HEIGHT-10, BLACK);
+                display.drawRect(ceil(SCREEN_WIDTH*(i-1)/3.0)+2, 2, SCREEN_WIDTH/3-4, SCREEN_HEIGHT-BIG_CHAR_HEIGHT-12, BLACK);
             }
         }
     }
@@ -203,21 +199,21 @@ void update_display(){
 }
 
 
+// Function displays NO CHARGE error message while all channels are not charged
 void no_charge_message(){
-    update_display();
     char str[] = "NO CHARGE";
     
-    // Draw black rect in with display text
-    display.fillRect(0, 8+BIG_CHAR_HEIGHT, SCREEN_WIDTH, SCREEN_HEIGHT - 2*(8+BIG_CHAR_HEIGHT), BLACK);
+    display.clearDisplay();
     display.setCursor(calcStrPosX(str, 1, 2), (SCREEN_HEIGHT+BIG_CHAR_HEIGHT)/2);
     display.print(str);
     display.display(); 
 
-    delay(1000);
+    delay(NO_CHARGE_MESSAGE_DISPLAY_TIME);
     update_display();
 }
 
 
+// Functions goes to next cycle and checks if it valid - selected chanel is charged
 bool find_valid_cycle(){
     // Cycle to next cycle
     if(curr_channel_cycle < 6){
@@ -236,6 +232,7 @@ bool find_valid_cycle(){
 }
 
 
+// Function opens MEASURE_PERMIT relay for as short time as possible to not discharge capacitors and takes measure
 void take_measure(){
     delay(2*RELAY_SWITCHING_TIME);          // Just to be sure no voltage or current spike form closing realys goes onto microcontroller 
     digitalWrite(MEASURE_PERMIT_PIN, HIGH);
@@ -249,8 +246,6 @@ void take_measure(){
 }
 
 
-
-
 void setup(){
     Serial.begin(115200);
 
@@ -260,6 +255,7 @@ void setup(){
         for(;;){}; 
     }
 
+    // Initiate all of the pins
     pinMode(CH1_MEASURE_PIN, INPUT);
     pinMode(CH2_MEASURE_PIN, INPUT);
     pinMode(CH3_MEASURE_PIN, INPUT);
@@ -282,7 +278,7 @@ void setup(){
 }
 
 void loop() {  
-    // Long press
+    // No need to do anthing unless button is pressed
     if(buttonPressed){      // If user didn't unpress button untill LONG_PRESS_DEALY passed then it is long press
         currTime = millis();
         if(currTime - lastTime > LONG_PRESS_DELAY && digitalRead(BUTTON_PIN) == HIGH){ // Long press
@@ -302,18 +298,31 @@ void loop() {
 
             take_measure();
             update_display();
-            buttonPressed = false;
+            // If we have used current channel or just charged and updated measurments
+            // do a short press automatically to switch to another channel
+            if(!((channel[0].selected && channel[0].charge < MIN_ACCEPTED_CHARGE)||
+                (channel[1].selected && channel[1].charge < MIN_ACCEPTED_CHARGE) ||
+                (channel[2].selected && channel[2].charge < MIN_ACCEPTED_CHARGE))&&
+                !(!channel[0].selected && !channel[1].selected && !channel[2].selected)){
+                buttonPressed = false;  // If we don't unpress button, short press will do automatically
+            }
 
         // Short press 
         } else if(digitalRead(BUTTON_PIN) == LOW){  // If button was unpressed before LONG_PRESS_DEALY passed then it is short press
 
-            // If no channel are charged display "NO CHARGE" message
-            if(channel[0].value < MIN_ACCEPTED_CHARGE &&
-               channel[1].value < MIN_ACCEPTED_CHARGE &&
-               channel[2].value < MIN_ACCEPTED_CHARGE){
-                no_charge_message();
+            // If no channel is charged, display "NO CHARGE" message and don't dispaly selected channel
+            if(channel[0].charge < MIN_ACCEPTED_CHARGE &&
+                channel[1].charge < MIN_ACCEPTED_CHARGE &&
+                channel[2].charge < MIN_ACCEPTED_CHARGE){
+
+                channel[0].selected = 0;
+                channel[1].selected = 0;
+                channel[2].selected = 0;
+                curr_channel_cycle = 0;
+                no_charge_message();  
+
             } else {
-                // Cycle through valid options, if found one then select channels according to it
+                // Cycle through valid options, if found one then selectS channels according to it
                 while(find_valid_cycle() == false){}
                 
                 channel[0].selected = channel_cycle[curr_channel_cycle][0];
@@ -322,7 +331,7 @@ void loop() {
 
                 update_display();
             }
+            buttonPressed = false;
         }
-        
     }
 }
